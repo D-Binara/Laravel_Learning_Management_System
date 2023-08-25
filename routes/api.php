87f66f3fe05de780d5489edb0d9d530e3e7181e1
D_Binara <dbkaveesha@gmail.com>
2023-08-25 22:40:02 +0530

    update

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::get('/asd', function (){
-    return "Asd0";
-});
+
