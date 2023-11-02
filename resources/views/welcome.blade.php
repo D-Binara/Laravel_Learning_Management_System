13aed799121e59daf205c5472b740f2002f39749
D_Binara <dbkaveesha@gmail.com>
2023-11-02 23:54:28 +0530

    update frontend.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,8 @@
 
 
     <div style="border-color: #1a1d20">
-        <div class="container1">
-            <img src="{{ URL::to('/') }}/images_folder/lms.png" alt="">
-        </div>
-        <h1 class="h11">Welcome to LMS</h1>
+        <img class="lms" src="{{ URL::to('/') }}/images_folder/lms_01.webp" alt="">
+        <h1>Welcome to LMS</h1>
     </div>
 
 @endsection
