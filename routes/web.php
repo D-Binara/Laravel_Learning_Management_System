25c16360b268e6bf9e19526271e1b6b9de0fc319
D_Binara <dbkaveesha@gmail.com>
2023-11-08 21:29:40 +0530

    update admin

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'indexAdmin'])->name('admin');
 
-Route::get('/about',[App\Http\Controllers\FrontendController::class, 'indexAbout']);
+
