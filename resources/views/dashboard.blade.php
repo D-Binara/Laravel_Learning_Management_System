8ef6d7252943e5c0f26f9e6ddcd3c485c5e3ddc5
D_Binara <dbkaveesha@gmail.com>
2023-11-20 23:23:50 +0530

    upadte dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,42 +4,21 @@
 
 @section('content')
 
+    <div class="row">
     @foreach($subjects as $sub)
         {{-- Subject cards --}}
-        <div class="row">
             <div class="card-gray col-4">
                 <div class="card">
                     <div class="card-header">
                         {{$sub->subject_name}}
                     </div>
                     <div class="card-body">
-                        <img href="" alt="SE">
+                        <img href="{{$sub->subject_image}}" alt="{{$sub->subject_name}}">
                     </div>
                 </div>
             </div>
-            <div class="card-gray col-4">
-                <div class="card">
-                    <div class="card-header">
-                        Software Engineer
-                    </div>
-                    <div class="card-body">
-                        <img href="" alt="SE">
-                    </div>
-                </div>
-            </div>
-            <div class="card-gray col-4">
-                <div class="card">
-                    <div class="card-header">
-                        Software Engineer
-                    </div>
-                    <div class="card-body">
-                        <img href="" alt="SE">
-                    </div>
-                </div>
-            </div>
-        </div>
     @endforeach
-
+    </div>
 @endsection
 
 
