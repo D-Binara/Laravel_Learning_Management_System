b1decd83e60b5972c157625b90a00e10e096832b
D_Binara <dbkaveesha@gmail.com>
2023-11-29 23:37:27 +0530

    update

--- resources/views/admin/addSubject.blade.php
+++ resources/views/admin/addSubject.blade.php
@@ -4,5 +4,22 @@
 
 @section('content')
 
-
+    <div class="col-md-8 ps-5" >
+        <form class="form-sample">
+            <h3>Add Subject</h3>
+            <label>Subject Name</label>
+            <label>
+                <input type="text" placeholder="Subject Name" class="display">
+            </label><br>
+            <label>Subject Credit</label>
+            <label>
+                <input type="number" placeholder="Subject Credit" class="display">
+            </label><br>
+            <label>Subject Name</label>
+            <label>
+                <input type="text" placeholder="Subject Name" class="display">
+            </label>
+        </form>
+    </div>
 @endsection
+
