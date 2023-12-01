6486e8a8b04a6f1445fb20766c4be074e1f46b2d
D_Binara <dbkaveesha@gmail.com>
2023-12-01 23:57:35 +0530

    change css

--- resources/views/admin/addSubject.blade.php
+++ resources/views/admin/addSubject.blade.php
@@ -4,22 +4,32 @@
 
 @section('content')
 
-    <div class="col-md-8 ps-5" >
-        <form class="form-sample">
-            <h3>Add Subject</h3>
-            <label>Subject Name</label>
-            <label>
-                <input type="text" placeholder="Subject Name" class="display">
-            </label><br>
-            <label>Subject Credit</label>
-            <label>
-                <input type="number" placeholder="Subject Credit" class="display">
-            </label><br>
-            <label>Subject Name</label>
-            <label>
-                <input type="text" placeholder="Subject Name" class="display">
-            </label>
-        </form>
+    <div class="col-sm-8 justify-content-center ">
+        <div class="card">
+            <div class="card-body ">
+                <form class="form-sample">
+                        <h3 class="text-center">Add Subject</h3>
+                    <div class="form-group">
+                        <label>Subject Name :-</label>
+                        <label>
+                            <input type="text" placeholder="Subject Name" class="form-control">
+                        </label><br>
+                    </div>
+                    <div class="form-group">
+                        <label>Subject Credit :-</label>
+                        <label>
+                            <input type="number" placeholder="Subject Credit" class="form-control">
+                        </label><br>
+                    </div>
+                    <div class="form-group">
+                        <label>Subject Image :-</label>
+                        <label>
+                            <input type="file" placeholder="Subject Name" class="form-control">
+                        </label>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 @endsection
 
